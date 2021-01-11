efca1970c1d75a2391cecb8bfd1ef364c6008b38
Ash-water <mizuwater78@gmail.com>
2021-01-11 11:15:55 +0900

    修正

--- app/javascript/packs/checked.js
+++ app/javascript/packs/checked.js
@@ -1,14 +1,14 @@
-function check(){
+function check() {
   const posts = document.querySelectorAll(".post");
-  posts.forEach(function(post){
+  posts.forEach(function (post) {
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
     post.addEventListener("click", () => {
-      if (post.getAttribute("data-load") != null) {
-        return null;
-      }
-      post.setAttribute("data-load", "true");
       const postId = post.getAttribute("data-id");
       const XHR = new XMLHttpRequest();
-      XHR.open('GET', `/posts/${postId}`, true);
+      XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
@@ -19,7 +19,7 @@ function check(){
         const item = XHR.response.post;
         if (item.checked === true) {
           post.setAttribute("data-check", "true");
-        } else if (item.checked === false){
+        } else if (item.checked === false) {
           post.removeAttribute("data-check");
         }
       };
@@ -27,4 +27,3 @@ function check(){
   });
 }
 setInterval(check, 1000);
-
